2041199cd1b309e580244c76d1bde9948da4ae43
Ebrahem <pilotebrahem2022@gmail.com>
2023-06-19 16:00:28 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+/**
+ * main - C
+ *
+ * Description: A C programing
+ *
+ *Return: Alawys 0 (Yes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
