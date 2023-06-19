5a99f059186e7e9b33bd3af24fc61309710aa4ff
Ebrahem <pilotebrahem2022@gmail.com>
2023-06-19 16:15:17 -0700

    f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - C
+ *
+ * Description: A C programing
+ *
+ *Return: Alawys 0 (Yes)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
