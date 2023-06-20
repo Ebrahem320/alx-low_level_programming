70553d342ea99d1fb74bc3a0189448d18255764e
Ebrahem <pilotebrahem2022@gmail.com>
2023-06-19 19:06:31 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - C
+ *
+ * Description: print low in C
+ *
+ *Return: Alawys 0 (Yes)
+*/
+
+int main(void)
+{
+        char ch = 'a';
+        char CH = 'A';
+
+        /*pr a - z*/
+        while (ch <= 'z')
+        {
+                putchar(ch);
+                ch++;
+        }
+
+        /*pr A - Z*/
+        while (CH <= 'Z')
+        {
+                putchar(CH);
+                CH++;
+        }
+        putchar('\n');
+
+        return (0);
+}
