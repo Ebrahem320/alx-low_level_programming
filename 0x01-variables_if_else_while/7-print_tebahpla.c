003e5f8de3937eab71bba3420f55e6c8c54462f1
Ebrahem <pilotebrahem2022@gmail.com>
2023-06-19 20:02:04 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - C
+ *
+ * Description: print a-z in C
+ *
+ *Return: Alawys 0 (Yes)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch => 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
