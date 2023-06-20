8090568d4301a977abbe0abefdd6cadef727aef2
Ebrahem <pilotebrahem2022@gmail.com>
2023-06-19 18:41:16 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - C
+ *
+ * Description: print all C
+ *
+ *Return: Alawys 0 (Yes)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		puthar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
